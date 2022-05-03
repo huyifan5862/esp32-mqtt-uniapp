9b6a44f4a713c12637804b8f17776050d9a300a7
“胡一帆” <h18235945862@163.com>
2022-05-03 17:45:46 +0800

    添加全推功能（未完成）

--- src/main/java/com/example/esp32mqtt/push/UnipushTest.java
+++ src/main/java/com/example/esp32mqtt/push/UnipushTest.java
@@ -15,11 +15,13 @@ import com.getui.push.v2.sdk.dto.req.message.android.Ups;
 import com.getui.push.v2.sdk.dto.req.message.ios.Alert;
 import com.getui.push.v2.sdk.dto.req.message.ios.Aps;
 import com.getui.push.v2.sdk.dto.req.message.ios.IosDTO;
+
 import java.util.Map;
 
 public class UnipushTest {
-    public static void main(String[] args) {
+    static PushApi pushApi;
 
+    static {
         GtApiConfiguration apiConfiguration = new GtApiConfiguration();
         //填写应用配置，参数在“Uni Push”下的“应用配置”页面中获取
         apiConfiguration.setAppId("4YakEFDJ9ZAe5d2jua7uJ5");
@@ -29,7 +31,10 @@ public class UnipushTest {
         // 实例化ApiHelper对象，用于创建接口对象
         ApiHelper apiHelper = ApiHelper.build(apiConfiguration);
         // 创建对象，建议复用。目前有PushApi、StatisticApi、UserApi
-        PushApi pushApi = apiHelper.creatApi(PushApi.class);
+        pushApi = apiHelper.creatApi(PushApi.class);
+    }
+
+    void pushToSingle() {
         //根据cid进行单推
         PushDTO<Audience> pushDTO = new PushDTO<Audience>();
         // 设置推送参数，requestid需要每次变化唯一
@@ -75,9 +80,7 @@ public class UnipushTest {
         iosDTO.setAps(aps);
         iosDTO.setType("notify");
         pushChannel.setIos(iosDTO);
-
         pushDTO.setPushChannel(pushChannel);
-
         // 进行cid单推
         ApiResult<Map<String, Map<String, String>>> apiResult = pushApi.pushToSingleByCid(pushDTO);
         if (apiResult.isSuccess()) {
@@ -87,5 +90,43 @@ public class UnipushTest {
             // failed
             System.out.println("code:" + apiResult.getCode() + ", msg: " + apiResult.getMsg());
         }
+    }
+
+    public void pushAll() {
+        PushDTO<String> pushDTO = new PushDTO<String>();
+        pushDTO.setRequestId(System.currentTimeMillis() + "");
+        Settings settings = new Settings();
+        pushDTO.setSettings(settings);
+        //消息有效期，走厂商消息必须设置该值
+        settings.setTtl(3600000);
+
+        //在线走个推通道时推送的消息体
+        PushMessage pushMessage = new PushMessage();
+        pushDTO.setPushMessage(pushMessage);
+        //此格式的透传消息由 unipush 做了特殊处理，会自动展示通知栏。开发者也可自定义其它格式，在客户端自己处理。
+        pushMessage.setTransmission(" {title:\"标题\",content:\"内容\",payload:\"自定义数据\"}");
+
+        //设置离线推送时的消息体
+        PushChannel pushChannel = new PushChannel();
+        //安卓离线厂商通道推送的消息体
+        AndroidDTO androidDTO = new AndroidDTO();
+        Ups ups = new Ups();
+        ThirdNotification thirdNotification = new ThirdNotification();
+        ups.setNotification(thirdNotification);
+        thirdNotification.setTitle("安卓离线展示的标题");
+        thirdNotification.setBody("安卓离线展示的内容");
+        thirdNotification.setClickType("intent");
+        //注意：intent参数必须按下方文档（特殊参数说明）要求的固定格式传值，intent错误会导致客户端无法收到消息
+        thirdNotification.setIntent("请填写固定格式的intent");
+        androidDTO.setUps(ups);
+        pushChannel.setAndroid(androidDTO);
+        pushDTO.setPushChannel(pushChannel);
+
+        pushApi.pushAll(pushDTO);
+    }
+
+    public static void main(String[] args) {
+        UnipushTest unipushTest = new UnipushTest();
+        unipushTest.pushAll();
     }
 }
